8307daa8ef298ed5ddbc97301d81bfda4ba956fe
rogaloana <96432543+ana87ana@users.noreply.github.com>
2024-05-13 21:13:43 +0200

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,40 +1,40 @@
 
 <template>
-    <h1>MyMusicMinutes</h1>
-    <div class="login-form">
-      <form @submit.prevent="submitForm">
-        <div class="form-group">
-          <label for="email"><span class="a">Email</span></label>
-          <input type="text" id="email" v-model="email" required>
-        </div>
-        <div class="form-group">
-          <label for="password"><span class="a">Password</span></label>
-          <input type="password" id="password" v-model="password" required>
-        </div>
-        <button type="submit">Login</button>
-        <button type="submit">Login as Admin</button>
-      </form>
-    </div>
-    <h3>Nemate račun? Stvorite ga <router-link to="/signup">ovdje!</router-link></h3>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        email: '',
-        password: ''
-      };
-    },
-    methods: {
-      submitForm() {
-        console.log('Email:', this.email);
-        console.log('Password:', this.password);
-      }
+  <h1>MyMusicMinutes</h1>
+  <div class="login-form">
+    <form @submit.prevent="submitForm">
+      <div class="form-group">
+        <label for="email"><span class="a">Email</span></label>
+        <input type="text" id="email" v-model="email" required>
+      </div>
+      <div class="form-group">
+        <label for="password"><span class="a">Password</span></label>
+        <input type="password" id="password" v-model="password" required>
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  </div>
+  <h3>Nemate račun? Stvorite ga <router-link to="/signup">ovdje!</router-link></h3>
+</template>
+
+<script> 
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    };
+  },
+  methods: {
+    submitForm() {
+      console.log('Email:', this.email);
+      console.log('Password:', this.password);
+      this.$router.push('/account')
     }
-  };
-  </script>
-  
+  }
+};
+</script>
+
   <style>
   
   h1{
@@ -94,3 +94,4 @@
     align-content: center;
   }
   </style>
+
