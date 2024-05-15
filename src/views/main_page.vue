8d95fd6c94e7881a6eab9bc2ade8c589208d8e56
rogaloana <96432543+ana87ana@users.noreply.github.com>
2024-05-15 22:26:28 +0200

    Add files via upload

---
+++ src/views/main_page.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="header">
+      <div class="title">Dobrodišli natrag!
+        Za što ste zainteresirani danas?
+      </div>
+      </div>
+      <div class="buttons">
+        <router-link to="/songs">
+          <button>SONGS</button>
+        </router-link>
+        <router-link to="/events">
+          <button>EVENTS</button>
+        </router-link>
+      </div>
+      <div class="account-link">
+        <router-link to="/account">
+          <div class="circle"></div>
+        </router-link>
+      </div>
+    
+  </template>
+  
+  <script>
+  export default {
+    // Vue.js component options
+  };
+  </script>
+  
+  <style scoped>
+  .header {
+    display: flex;
+    justify-content: center;
+    align-content:center;
+    padding: 10px;
+    margin-top: 50px;
+  }
+  
+  .title {
+    font-size: 50px;
+    color:#f4c8ca
+  }
+  
+  .buttons button {
+    background-color: #640d12;
+    margin-left: 40px;
+    margin-right: 40px;
+    margin-top: 80px;
+    padding-top: 50px;
+    padding-bottom: 50px;
+    padding-left: 35px;
+    padding-right: 35px;
+    font-size: 35px;
+    color:#f4c8ca;
+  }   
+
+  
+  .circle {
+    position: absolute;
+    top: 15px;
+    right: 15px;
+    width: 55px;
+    height: 55px;
+    background-color: #f4c8ca;
+    border-radius: 50%;
+    cursor: pointer;
+  }
+  
+  
+  </style>
+  
---
+++ src/views/songs.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="filter-page">
+      <div class="filter-box left">
+        <h2>Mood</h2>
+        <div v-for="(item, index) in moods" :key="index">
+          <input type="radio" name="mood" :id="'mood-radio-' + index" v-model="selectedMood" :value="item">
+          <label :for="'mood-radio-' + index">{{ item }}</label>
+        </div>
+      </div>
+      <div class="filter-box right">
+        <h2>Occasion</h2>
+        <div v-for="(item, index) in occasions" :key="index">
+          <input type="radio" name="occasion" :id="'occasion-radio-' + index" v-model="selectedOccasion" :value="item">
+          <label :for="'occasion-radio-' + index">{{ item }}</label>
+        </div>
+      </div>
+      <button @click="submitFilters">Submit</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    
+    data() {
+      return {
+        moods: ['Happy', 'Sad', 'Excited', 'Calm'], // Example moods
+        occasions: ['Party', 'Study', 'Workout', 'Relax'], // Example occasions
+        selectedMood: null,
+        selectedOccasion: null
+      };
+    },
+    methods: {
+      submitFilters() {
+        if (this.selectedMood && this.selectedOccasion) {
+          // Here you would send the selected mood and occasion to Firebase
+          // For simplicity, let's log the selected values for now
+          console.log("Selected Mood:", this.selectedMood);
+          console.log("Selected Occasion:", this.selectedOccasion);
+          
+          // After sending data to Firebase, navigate to the page listing songs
+          this.$router.push('/song-list');
+        } else {
+          alert("Please select a mood and an occasion.");
+        }
+      }
+    }
+  };
+  </script>
+  
+  <style>
+  .filter-page {
+    display: flex;
+    justify-content: space-between;
+  }
+  
+  .filter-box {
+    width: 45%;
+    border: 1px solid #ccc;
+    padding: 10px;
+  }
+  
+  .filter-box h2 {
+    margin-top: 0;
+  }
+  
+  .filter-box label {
+    display: block;
+    margin-bottom: 5px;
+  }
+  
+  button {
+    margin-top: 20px;
+  }
+  </style>
+  
