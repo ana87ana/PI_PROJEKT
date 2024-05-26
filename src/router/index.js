59a9bb4657b3ee63c2edc49f5a4766876892557f
rogaloana <96432543+ana87ana@users.noreply.github.com>
2024-05-26 19:48:38 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import Login from '@/views/Login.vue'
+import Songs from '@/views/songs.vue';
+import SongsFiltered from '@/views/songs_filtered.vue';
 
 const routes = [
   {
@@ -15,6 +16,43 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Signup.vue')
+  },
+  {
+    path: '/account',
+    name: 'account',
+    component: () => import('../views/accout_page.vue')
+  },
+  {
+    path: '/mainpage',
+    name: 'mainpage',
+    component: () => import('@/views/main_page.vue')
+  },
+  {
+    path: '/songs',
+    name: 'songs',
+    component: () => import('@/views/songs.vue')
+  },
+  {
+    path: '/top10',
+    name: 'top10',
+    component: () => import('@/views/top10.vue')
+  },
+  {
+    path: '/firebase',
+    name: 'firebase',
+    component: () => import('@/views/firebase.js')
+  },
+  {
+    path: '/songs_filtered',
+    name: 'songs_filtered',
+    component: () => import('@/views/songs_filtered.vue')
+  },
+  { path: '/', component: Songs },
+  { path: '/songs_filtered', component: SongsFiltered },
+  {
+    path: '/post_event',
+    name: 'post_event',
+    component: () => import('@/views/post_event.vue')
   }
 ]
 
@@ -24,3 +62,4 @@ const router = createRouter({
 })
 
 export default router
+
