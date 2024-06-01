ed198886670250d1dfc238d3832f75bba7bbe7e7
rogaloana <96432543+ana87ana@users.noreply.github.com>
2024-06-01 21:43:51 +0200

    Update main_page.vue

--- src/views/main_page.vue
+++ src/views/main_page.vue
@@ -1,70 +1,91 @@
 <template>
-    <div class="header">
-      <div class="title">Dobrodišli natrag!
-        Za što ste zainteresirani danas?
-      </div>
-      </div>
-      <div class="buttons">
-        <router-link to="/songs">
-          <button>SONGS</button>
-        </router-link>
-        <router-link to="/events">
-          <button>EVENTS</button>
-        </router-link>
-      </div>
-      <div class="account-link">
-        <router-link to="/account">
-          <div class="circle"></div>
-        </router-link>
-      </div>
-    
-  </template>
-  
-  <script>
-  export default {
-    // Vue.js component options
-  };
-  </script>
-  
-  <style scoped>
-  .header {
-    display: flex;
-    justify-content: center;
-    align-content:center;
-    padding: 10px;
-    margin-top: 50px;
-  }
-  
-  .title {
-    font-size: 50px;
-    color:#f4c8ca
-  }
-  
-  .buttons button {
-    background-color: #640d12;
-    margin-left: 40px;
-    margin-right: 40px;
-    margin-top: 80px;
-    padding-top: 50px;
-    padding-bottom: 50px;
-    padding-left: 35px;
-    padding-right: 35px;
-    font-size: 35px;
-    color:#f4c8ca;
-  }   
+  <div class="header">
+    <div class="title">Dobrodišli natrag! Za što ste zainteresirani danas?</div>
+  </div>
+  <div class="buttons">
+    <button @click="redirectToSongs">SONGS</button>
+    <router-link to="/events">
+      <button>EVENTS</button>
+    </router-link>
+  </div>
+  <div class="account-link">
+    <router-link to="/account">
+      <div class="circle"></div>
+    </router-link>
+  </div>
+</template>
 
-  
-  .circle {
-    position: absolute;
-    top: 15px;
-    right: 15px;
-    width: 55px;
-    height: 55px;
-    background-color: #f4c8ca;
-    border-radius: 50%;
-    cursor: pointer;
-  }
-  
-  
-  </style>
-  
+<script>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { auth, db } from '@/views/firebase';
+import { getDoc, doc } from 'firebase/firestore';
+
+export default {
+  setup() {
+    const isAdmin = ref(false);
+    const router = useRouter();
+
+    const redirectToSongs = () => {
+      if (isAdmin.value) {
+        router.push('/songs_admin');
+      } else {
+        router.push('/songs');
+      }
+    };
+
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const userDoc = await getDoc(doc(db, 'korisnik', user.uid));
+        if (userDoc.exists()) {
+          isAdmin.value = userDoc.data().isAdmin;
+        }
+      }
+    });
+
+    return {
+      isAdmin,
+      redirectToSongs,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  padding: 10px;
+  margin-top: 50px;
+}
+
+.title {
+  font-size: 50px;
+  color: #f4c8ca;
+}
+
+.buttons button {
+  background-color: #640d12;
+  margin-left: 15%;
+  margin-right: 15%;
+  margin-top: 80px;
+  padding-top: 50px;
+  padding-bottom: 50px;
+  padding-left: 60px;
+  padding-right: 200px;
+  font-size: 35px;
+  color: #f4c8ca;
+}
+
+.circle {
+  position: absolute;
+  top: 15px;
+  right: 15px;
+  width: 55px;
+  height: 55px;
+  background-color: #f4c8ca;
+  border-radius: 50%;
+  cursor: pointer;
+}
+</style>
